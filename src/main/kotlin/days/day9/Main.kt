f31989638d1fbb6676651fd197fb7646c313a5f4
Jared <jaredgraham1998@gmail.com>
2023-12-21 18:17:33 -0600

    day9b

--- src/main/kotlin/days/day9/Main.kt
+++ src/main/kotlin/days/day9/Main.kt
@@ -3,7 +3,7 @@ package days.day9
 import java.io.File
 
 fun main() {
-    partOne()
+    partTwo()
 }
 
 fun partOne() {
@@ -38,5 +38,32 @@ fun getDerivative(history: List<Long>): Long {
 }
 
 fun partTwo() {
+    val histories = File("./src/main/kotlin/days/day9/input.txt").readLines().map {
+        Regex("""(-*\w+)""").findAll(it).map { match -> match.groups[1]?.value?.toLong() ?: -1 }.toList()
+    }
+    println(histories.fold(0L) { acc, next ->
+        acc + getDerivativeBackwards(next) })
+}
 
+fun getDerivativeBackwards(history: List<Long>): Long {
+    val derivatives = mutableListOf(history)
+    do {
+        val newDerivative = derivatives.last().mapIndexed { index, next ->
+            if (index < derivatives.last().size - 1) {
+                return@mapIndexed derivatives.last()[index + 1] - next
+            } else {
+                -1
+            }
+        }.dropLast(1)
+
+        if (newDerivative.isNotEmpty()) {
+            derivatives.add(newDerivative)
+        } else {
+            derivatives.add(listOf(0L))
+        }
+    } while (!derivatives.last().all { it == 0L })
+
+    return derivatives.reversed().fold(0L) { acc, next ->
+        next.first() - acc
+    }
 }
